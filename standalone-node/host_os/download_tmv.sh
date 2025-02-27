d39b07019dc02828e4841e95bb18ccb5a41884a1
ANAND B JYOTI <anand.b.jyoti@intel.com>
2025-02-27 18:38:26 +0530

    Adding script to download the Tiber Microvisor artifacts (#4)
    Signed-off-by: Anand B Jyoti <anand.b.jyoti@intel.com>

---
+++ standalone-node/host_os/download_tmv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+# Download the tiber Microvisor from open source no-auth file server
+# The file server URL is defined in FILE_RS_URL
+FILE_RS_URL="https://af01p-png.devtools.intel.com"
+TMV_VERSION=3.0
+TMV_BUILD_DATE=20250219
+TMV_BUILD_NO=2225
+TMV_FILE_NAME="tiber-readonly-${TMV_VERSION}.${TMV_BUILD_DATE}.${TMV_BUILD_NO}-signed"
+TMV_RAW_GZ="${TMV_FILE_NAME}.raw.gz"
+TMV_DER="${TMV_FILE_NAME}.der"
+TMV_SHA256SUM="${TMV_FILE_NAME}.raw.gz.sha256sum"
+
+wget --no-proxy --no-check-certificate ${FILE_RS_URL}/artifactory/tiberos-png-local/non-rt/${TMV_VERSION}/${TMV_BUILD_DATE}.${TMV_BUILD_NO}/${TMV_RAW_GZ} -O tiber_microvisor.raw.gz
+wget --no-proxy --no-check-certificate ${FILE_RS_URL}/artifactory/tiberos-png-local/non-rt/${TMV_VERSION}/${TMV_BUILD_DATE}.${TMV_BUILD_NO}/${TMV_DER} -O tiber_microvisor.der
+wget --no-proxy --no-check-certificate ${FILE_RS_URL}/artifactory/tiberos-png-local/non-rt/${TMV_VERSION}/${TMV_BUILD_DATE}.${TMV_BUILD_NO}/${TMV_SHA256SUM} -O tiber_microvisor.raw.gz.sha256sum
--- standalone-node/host_os/readme.md
+++ standalone-node/host_os/readme.md
@@ -1,1 +1,20 @@
-Created for folders creation.
+# Host OS Folder
+
+This folder is created for managing and organizing various host operating system related files and configurations.
+
+## Contents
+
+- Configuration files
+- Scripts
+- Documentation
+
+## Usage
+Execute the download_tmv.sh script to download the Tiber Microvisor OS, sh256sum and der files.
+
+## Contributing
+
+Feel free to contribute by adding more scripts, configurations, or improving the documentation.
+
+## License
+
+This project is licensed under the Apache 2.0 License.
