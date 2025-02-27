f63f1e18624391125601cd30b51f377f1c14ca33
ANAND B JYOTI <anand.b.jyoti@intel.com>
2025-02-27 19:35:56 +0530

    Added checksum verification (#6)
    Initial code...

--- standalone-node/host_os/download_tmv.sh
+++ standalone-node/host_os/download_tmv.sh
@@ -16,3 +16,15 @@ TMV_SHA256SUM="${TMV_FILE_NAME}.raw.gz.sha256sum"
 wget --no-proxy --no-check-certificate ${FILE_RS_URL}/artifactory/tiberos-png-local/non-rt/${TMV_VERSION}/${TMV_BUILD_DATE}.${TMV_BUILD_NO}/${TMV_RAW_GZ} -O tiber_microvisor.raw.gz
 wget --no-proxy --no-check-certificate ${FILE_RS_URL}/artifactory/tiberos-png-local/non-rt/${TMV_VERSION}/${TMV_BUILD_DATE}.${TMV_BUILD_NO}/${TMV_DER} -O tiber_microvisor.der
 wget --no-proxy --no-check-certificate ${FILE_RS_URL}/artifactory/tiberos-png-local/non-rt/${TMV_VERSION}/${TMV_BUILD_DATE}.${TMV_BUILD_NO}/${TMV_SHA256SUM} -O tiber_microvisor.raw.gz.sha256sum
+
+# Verify the SHA256 checksum
+echo "Verifying SHA256 checksum..."
+EXPECTED_CHECKSUM=$(awk '{print $1}' tiber_microvisor.raw.gz.sha256sum)
+ACTUAL_CHECKSUM=$(sha256sum tiber_microvisor.raw.gz | awk '{print $1}')
+
+if [ "$EXPECTED_CHECKSUM" == "$ACTUAL_CHECKSUM" ]; then
+    echo "SHA256 checksum verification passed."
+else
+    echo "SHA256 checksum verification failed!" >&2
+    exit 1
+fi
