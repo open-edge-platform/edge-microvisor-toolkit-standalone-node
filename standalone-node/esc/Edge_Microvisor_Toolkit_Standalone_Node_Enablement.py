2478b3332bb8e3cab371f9d3a9b209c7a8a4088a
punam20 <102661080+punam20@users.noreply.github.com>
2025-04-08 20:56:16 +0530

    Fix logs and password user input (#49)
    https://jira.devtools.intel.com/browse/ITEP-25701
    
    Signed-off-by: punam biswal <punam.biswal@intel.com>

--- standalone-node/esc/Edge_Microvisor_Toolkit_Standalone_Node_Enablement.py
+++ standalone-node/esc/Edge_Microvisor_Toolkit_Standalone_Node_Enablement.py
@@ -9,6 +9,9 @@ from subprocess import run, PIPE
 from termcolor import colored
 from esb_common.logger import Logger
 from esb_common import locale
+import sys
+import tty
+import termios
 
 MODULE = "Edge Microvisor Toolkit Standalone Node Script"
 CURRENT_DIR = os.getcwd()
@@ -69,7 +72,6 @@ class EdgeMicroVisorToolKitStandaloneNodeDeployment(object):
         try:
             self.main_install()
             self.main_post_install()
-            self.log.info("Successfully installed {0}".format(self.module))
         except EdgeMicroVisorStandaloneNodeDeploymentException as error:
             if error.args[0]:
                 if isinstance(error.args[0], bytes):
@@ -98,6 +100,11 @@ class EdgeMicroVisorToolKitStandaloneNodeDeployment(object):
         finally:
             self.cleanup()
             os.chdir(CURRENT_DIR)
+
+        if status:
+            self.log.info("Successfully installed {0}".format(self.module))
+        else:
+            self.log.error("Installation failed for {0}".format(self.module))
         return status
 
     # Define a function to validate the proxy format using regex
@@ -123,31 +130,38 @@ class EdgeMicroVisorToolKitStandaloneNodeDeployment(object):
 
     def update_proxy_file(self, http_proxy, https_proxy, no_proxy, ssh_key,
                           user_name, password, file_path):
-        with open(file_path, 'w') as file:
-            file.write(f'http_proxy="{http_proxy}"\n')
-            file.write(f'https_proxy="{https_proxy}"\n')
-            file.write(f'no_proxy="{no_proxy}"\n')
-            file.write(f'HTTP_PROXY="{http_proxy}"\n')
-            file.write(f'HTTPS_PROXY="{https_proxy}"\n')
-            file.write(f'NO_PROXY="{no_proxy}"\n')
-            file.write(f'ssh_key="{ssh_key}"\n')
-            file.write(f'user_name="{user_name}"\n')
-            file.write(f'passwd="{password}"\n')
+        try:
+            with open(file_path, 'w') as file:
+                file.write(f'http_proxy="{http_proxy}"\n')
+                file.write(f'https_proxy="{https_proxy}"\n')
+                file.write(f'no_proxy="{no_proxy}"\n')
+                file.write(f'HTTP_PROXY="{http_proxy}"\n')
+                file.write(f'HTTPS_PROXY="{https_proxy}"\n')
+                file.write(f'NO_PROXY="{no_proxy}"\n')
+                file.write(f'ssh_key="{ssh_key}"\n')
+                file.write(f'user_name="{user_name}"\n')
+                file.write(f'passwd="{password}"\n')
+        except FileNotFoundError:
+            self.log.error("Configuration file not found.")
 
     def run_lsblk(self):
         try:
             # Run the lsblk command with filtering for disks
-            result = run(
-                ['lsblk', '-o', 'NAME,SIZE,TYPE,MODEL', '|', 'grep', 'disk'],
-                stdout=PIPE, stderr=PIPE, text=True, shell=True
-            )
+            command = ["lsblk", "-o", "NAME,SIZE,TYPE,MODEL"]
+            result = run(command, stdout=PIPE,
+                         stderr=PIPE, text=True, shell=False)
             if result.returncode == 0:
-                return result.stdout
+                # Filter output for disks
+                disks = [line for line in result.stdout.splitlines()
+                         if "disk" in line]
+                return "\n".join(disks)
             else:
-                print(colored("Error running lsblk:", result.stderr, "red"))
+                print(colored("Error running lsblk:", "red"))
+                self.log.error(result.stderr)
                 return None
         except Exception as e:
             print(f"Error: {e}")
+            self.log.error(f"Exception occurred: {e}")
             return None
 
     def install_standalone_edge_node_setup_script(self):
@@ -167,144 +181,144 @@ class EdgeMicroVisorToolKitStandaloneNodeDeployment(object):
 
         print("-------+----+-- Installation Begins --+----+-------")
         self.log.info("Installation begins.")
-        try:
-            # Search for sen-installation-files.tar.gz
-            sen_file_path = None
-            for dirpath, dirnames, filenames in os.walk(CURRENT_DIR):
-                for filename in filenames:
-                    if filename == "sen-installation-files.tar.gz":
-                        sen_file_path = os.path.join(dirpath, filename)
-                if sen_file_path:
-                    break
 
-            if not sen_file_path:
-                self.log.error("File sen-installation-files.tar.gz not found.")
-                print(colored(
-                    "ERROR: File sen-installation-files.tar.gz not found.",
-                    "red"))
-                raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                    "File sen-installation-files.tar.gz not found"
-                )
+        # Search for sen-installation-files.tar.gz
+        sen_file_path = None
+        for dirpath, dirnames, filenames in os.walk(CURRENT_DIR):
+            for filename in filenames:
+                if filename == "sen-installation-files.tar.gz":
+                    sen_file_path = os.path.join(dirpath, filename)
+            if sen_file_path:
+                break
 
-            command = "tar -xvf {0}".format(sen_file_path)
-            status = run(
-                command.split(), stdout=PIPE, stderr=PIPE, shell=False)
-            if status.returncode:
-                raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                    status.stdout + status.stderr,
-                    "{0} to unzip".format(locale.FAILED),
-                    command
-                )
-
-            files_to_check = [
-                'config-file',
-                'bootable-usb-prepare.sh',
-                'usb-bootable-files.tar.gz'
-                ]
-
-            # Get the list of files in the current directory
-            current_directory_files = os.listdir(CURRENT_DIR)
-
-            # Check for the presence of required file
-            for file_name in files_to_check:
-                if file_name in current_directory_files:
-                    self.log.info(f"{file_name} is present in directory.")
-                else:
-                    raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                        f"{file_name} is not present.")
-
-            # Get user inputs for the proxies, ssh key, user name and password
-            http_proxy = input(colored(
-                'Enter the HTTP proxy (leave blank for none): ', "green"))
-            if http_proxy and not self.validate_proxy(http_proxy):
-                self.log.error("Invalid HTTP proxy format. Exiting...")
-                print(colored('Invalid HTTP proxy format. Exiting...', "red"))
-                raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                    "Invalid HTTP proxy format.")
-
-            https_proxy = input(colored(
-                'Enter the HTTPS proxy (leave blank for none): ', "green"))
-            if https_proxy and not self.validate_proxy(https_proxy):
-                self.log.error("Invalid HTTPS proxy format. Exiting...")
-                print(colored('Invalid HTTPS proxy format. Exiting...', "red"))
-                raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                    "Invalid HTTPS proxy format.")
-
-            no_proxy = input(colored(
-                'Enter the NO_PROXY list (comma-separated): ',
-                "green"))
-            if not self.validate_no_proxy(no_proxy):
-                self.log.error("Invalid NO_PROXY format. Exiting...")
-                print(colored('Invalid NO_PROXY format. Exiting...', "red"))
-                raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                    "Invalid NO_PROXY format.")
-
-            ssh_key = input(colored('Enter your SSH public key: ', "green"))
-            if not self.validate_ssh_key(ssh_key):
-                self.log.error("Invalid SSH key format. Exiting...")
-                print(colored('Invalid SSH key format. Exiting...', "red"))
-                raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                    "Invalid SSH key format.")
-
-            user_name = input(colored('Enter user name: ', "green"))
-            password = input(colored('Enter password: ', "green"))
-            if not user_name or not password:
-                raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                    "User name or password cannot be empty.")
-
-            proxy_ssh_config_path = os.path.join(
-                CURRENT_DIR, "config-file"
-            )
-            self.update_proxy_file(
-                http_proxy, https_proxy, no_proxy, ssh_key,
-                user_name, password, proxy_ssh_config_path
-            )
+        if not sen_file_path:
+            self.log.error("File sen-installation-files.tar.gz not found.")
             print(colored(
-                'proxy_ssh_config File updated successfully!', "green"))
-
-            lsblk_output = self.run_lsblk()
-            if lsblk_output:
-                print(colored("Disk Information:\n", "green"))
-                print(lsblk_output)
-
-            disk = input(colored(
-                "Enter the disk (e.g., /dev/sda, /dev/sdb): ",
-                "green").strip()
-                )
-            print(colored(f"You selected the disk: {disk}", "green"))
-
-            command = (
-                f"sudo ./bootable-usb-prepare.sh {disk}"
-                " usb-bootable-files.tar.gz"
-                " config-file > usb_preparation.log 2>&1"
+                "ERROR: File sen-installation-files.tar.gz not found.",
+                "red"))
+            raise EdgeMicroVisorStandaloneNodeDeploymentException(
+                "File sen-installation-files.tar.gz not found"
             )
-            result = run(command, shell=True, text=True)
-            if result.returncode == 0:
-                print(colored(
-                    "Bootable USB preparation completed successfully!", "green"
-                ))
-                self.log.info(
-                    "Bootable USB preparation completed successfully."
-                    )
+
+        command = "tar -xvf {0}".format(sen_file_path)
+        status = run(
+            command.split(), stdout=PIPE, stderr=PIPE, shell=False)
+        if status.returncode:
+            self.log.error("Error during tar extraction.")
+            self.log.error(
+                f"Standard Output: {status.stdout.decode('utf-8')}"
+            )
+            self.log.error(
+                f"Standard Error: {status.stderr.decode('utf-8')}"
+            )
+            raise EdgeMicroVisorStandaloneNodeDeploymentException(
+                status.stdout + status.stderr,
+                "{0} to unzip".format(locale.FAILED),
+                command
+            )
+
+        files_to_check = [
+            'config-file',
+            'bootable-usb-prepare.sh',
+            'usb-bootable-files.tar.gz',
+            'edgenode-logs-collection.sh'
+            ]
+
+        # Get the list of files in the current directory
+        current_directory_files = os.listdir(CURRENT_DIR)
+
+        # Check for the presence of required file
+        for file_name in files_to_check:
+            if file_name in current_directory_files:
+                self.log.info(f"{file_name} is present in directory.")
             else:
-                self.log.error("Error during bootable USB preparation")
-                self.log.error(f"Standard Error: {result.stderr}")
-                print(colored(
-                    "Error during bootable USB preparation. "
-                    "See logs for details.", "red"
-                ))
                 raise EdgeMicroVisorStandaloneNodeDeploymentException(
-                    "Bootable USB preparation failed."
-                )
-        except KeyboardInterrupt:
-            print("{0} {1}".format(
-                colored("WARNING:", "yellow"), locale.USER_ABORTED))
-            self.log.warn(locale.USER_ABORTED)
-        except Exception as e:
-            self.log.error(f"An error occurred during the installation: {e}")
+                    f"{file_name} is not present.")
+
+        # Get user inputs for the proxies, ssh key, user name and password
+        http_proxy = input(colored(
+            'Enter the HTTP proxy (leave blank for none): ', "green"))
+        if http_proxy and not self.validate_proxy(http_proxy):
+            self.log.error("Invalid HTTP proxy format. Exiting...")
+            print(colored('Invalid HTTP proxy format. Exiting...', "red"))
+            raise EdgeMicroVisorStandaloneNodeDeploymentException(
+                "Invalid HTTP proxy format.")
+
+        https_proxy = input(colored(
+            'Enter the HTTPS proxy (leave blank for none): ', "green"))
+        if https_proxy and not self.validate_proxy(https_proxy):
+            self.log.error("Invalid HTTPS proxy format. Exiting...")
+            print(colored('Invalid HTTPS proxy format. Exiting...', "red"))
+            raise EdgeMicroVisorStandaloneNodeDeploymentException(
+                "Invalid HTTPS proxy format.")
+
+        no_proxy = input(colored(
+            'Enter the NO_PROXY list (comma-separated): ',
+            "green"))
+        if not self.validate_no_proxy(no_proxy):
+            self.log.error("Invalid NO_PROXY format. Exiting...")
+            print(colored('Invalid NO_PROXY format. Exiting...', "red"))
+            raise EdgeMicroVisorStandaloneNodeDeploymentException(
+                "Invalid NO_PROXY format.")
+
+        ssh_key = input(colored('Enter your SSH public key: ', "green"))
+        if not self.validate_ssh_key(ssh_key):
+            self.log.error("Invalid SSH key format. Exiting...")
+            print(colored('Invalid SSH key format. Exiting...', "red"))
+            raise EdgeMicroVisorStandaloneNodeDeploymentException(
+                "Invalid SSH key format.")
+
+        user_name = input(colored('Enter user name: ', "green"))
+        password = masked_input(colored('Enter password: ', "green"))
+        if not user_name or not password:
+            raise EdgeMicroVisorStandaloneNodeDeploymentException(
+                "User name or password cannot be empty.")
+
+        proxy_ssh_config_path = os.path.join(
+            CURRENT_DIR, "config-file"
+        )
+        self.update_proxy_file(
+            http_proxy, https_proxy, no_proxy, ssh_key,
+            user_name, password, proxy_ssh_config_path
+        )
+        print(colored(
+            'proxy_ssh_config File updated successfully!', "green"))
+
+        lsblk_output = self.run_lsblk()
+        if lsblk_output:
+            print(colored("Disk Information:\n", "green"))
+            print(lsblk_output)
+
+        disk = input(colored(
+            "Enter the disk (e.g., /dev/sda, /dev/sdb): ",
+            "green").strip()
+            )
+        print(colored(f"You selected the disk: {disk}", "green"))
+
+        print(colored("Starting bootable USB preparation...", "green"))
+        command = [
+            "sudo", "./bootable-usb-prepare.sh", disk,
+            "usb-bootable-files.tar.gz", "config-file"
+        ]
+        with open("usb_preparation.log", "w") as log_file:
+            result = run(command, stdout=log_file,
+                         stderr=log_file, shell=False, text=True)
+        if result.returncode == 0:
             print(colored(
-                f"An error occurred during the installation: {e}", "red"
+                "Bootable USB preparation completed successfully!", "green"
             ))
+            self.log.info(
+                "Bootable USB preparation completed successfully."
+            )
+        else:
+            self.log.error("Error during bootable USB preparation")
+            print(colored(
+                "Error during bootable USB preparation. "
+                "See logs for details.", "red"
+            ))
+            raise EdgeMicroVisorStandaloneNodeDeploymentException(
+                "Bootable USB preparation failed."
+            )
 
     def verify_installation(self):
         """
@@ -389,3 +403,34 @@ def main_uninstall(MODULE_PATH, OUTPUT_PATH, TYPE):
         return en_script_uninstall.SUCCESS
     else:
         return en_script_uninstall.FAILED
+
+
+def masked_input(prompt):
+    """
+    Custom implementation of getpass to display '*' for each character entered.
+        """
+    print(prompt, end='', flush=True)
+    password = ''
+    stdin_file_descriptor = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(stdin_file_descriptor)
+    try:
+        tty.setraw(stdin_file_descriptor)
+        while True:
+            char = sys.stdin.read(1)
+            if char == '\n' or char == '\r':  # Enter key
+                print('')
+                break
+            elif char == '\x7f':  # Backspace key
+                if len(password) > 0:
+                    password = password[:-1]
+                    print('\b \b', end='', flush=True)
+            else:
+                password += char
+                print('*', end='', flush=True)
+    except Exception as e:
+        print("\nAn error occurred while reading input.")
+        raise e
+    finally:
+        termios.tcsetattr(stdin_file_descriptor,
+                          termios.TCSADRAIN, old_settings)
+    return password
