392b361ed4197116d284e48b76a152721acfd3ae
punam biswal <punam.biswal@intel.com>
2025-06-02 10:20:11 +0530

    Multiple profile support
    profile based package build
    upload artifact to no auth registry.
    
    Signed-off-by: punam biswal <punam.biswal@intel.com>

---
+++ standalone-node/installation_scripts/get_image_name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+case "$1" in
+  non-rt-k8s) echo "non_rt/edge-readonly-3.0.20250413.2200-prod-signed" ;;
+  non-rt-docker) echo "non_rt/edge-readonly-3.0.20250601.2200" ;;
+  rt-docker) echo "rt/edge-readonly-rt-3.0.20250601.2058" ;;
+  *) echo "Unknown profile: $1" >&2; exit 1 ;;
+esac
