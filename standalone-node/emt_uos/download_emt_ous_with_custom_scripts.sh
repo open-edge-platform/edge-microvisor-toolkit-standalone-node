f8f281850791ce55ec7fe289aea0e98c51433c3c
sybacharan <syba.sai.charan@intel.com>
2025-06-09 18:27:17 +0530

    Update download_emt_ous_with_custom_scripts.sh

--- standalone-node/emt_uos/download_emt_ous_with_custom_scripts.sh
+++ standalone-node/emt_uos/download_emt_ous_with_custom_scripts.sh
@@ -15,7 +15,7 @@ EMT_FILE_NAME="emt_uos_image/emt_uos_x86_64_${EMT_BUILD_DATE}"
 EMT_RAW_GZ="${EMT_FILE_NAME}.tar.gz"
 
 
-wget -O edge_uos.tar.gz ${FILE_RS_URL}/${EMT_RAW_GZ}  
+wget --user="intel" --password="intel@123" -O edge_uos.tar.gz ${FILE_RS_URL}/${EMT_RAW_GZ}  
 
 if [ ! -d uOS ]; then
     mkdir -p uOS
