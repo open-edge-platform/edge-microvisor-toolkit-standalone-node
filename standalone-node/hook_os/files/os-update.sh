3d1431b24fa45501cf43be7646c6c93902787b58
Nayak, Ipsita <ipsita.nayak@intel.com>
2025-06-10 04:18:01 -0700

    Updated script

--- standalone-node/hook_os/files/os-update.sh
+++ standalone-node/hook_os/files/os-update.sh
@@ -22,35 +22,46 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Check if the correct number of arguments is provided
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 [-u <URL_to_Microvisor_image> <URL_to_SHA_file>] | <Direct_path_to_Microvisor_image> <SHA256_checksum>"
-    exit 1
-fi
 
 # Temporary directory for downloads
 TEMP_DIR="/tmp/microvisor-update"
 mkdir -p "$TEMP_DIR"
 check_success "Creating temporary directory"
 
-# Determine if URL mode is used
+# URL mode
 if [ "$1" == "-u" ]; then
-    if [ "$#" -ne 3 ]; then
-        echo "Usage: $0 -u <URL_to_Microvisor_image> <URL_to_SHA_file>"
-        exit 1
+    # Check if the correct number of arguments is provided for URL mode
+    if [ "$#" -ne 7 ]; then
+        error_exit "Usage: $0 -u <URL_to_Microvisor_image_base> <URL_to_SHA_file> -r <version> -v <build_version>"
     fi
 
     # URL mode
-    IMAGE_URL="$2"
+    IMAGE_BASE_URL="$2"
     SHA_ID="$3"
+    IMG_VER="$5"
+    IMAGE_BUILD="$7"
 
+    # Determine the domain and construct the IMAGE_URL accordingly
+    if [[ "$IMAGE_BASE_URL" == *"files-rs.edgeorchestration.intel.com"* ]]; then
+        IMAGE_URL="${IMAGE_BASE_URL}/edge-readonly-${IMG_VER}.${IMAGE_BUILD}-signed.raw.gz"
+    elif [[ "$IMAGE_BASE_URL" == *"af01p-png.devtools.intel.com"* ]]; then
+        IMAGE_URL="${IMAGE_BASE_URL}/${IMG_VER}/${IMAGE_BUILD}/edge-readonly-${IMG_VER}.${IMAGE_BUILD}-signed.raw.gz"
+    else
+        error_exit "Unsupported domain in URL: $IMAGE_BASE_URL"
+    fi
+
+    echo "Constructed IMAGE URL: $IMAGE_URL"
     # Download the Microvisor image
     IMAGE_PATH="$TEMP_DIR/edge_microvisor_toolkit.raw.gz"
     echo "Downloading microvisor image from $IMAGE_URL..."
-    curl -k "$IMAGE_URL" -o "$IMAGE_PATH"
-    check_success "Downloading microvisor image"
+    curl -k "$IMAGE_URL" -o "$IMAGE_PATH" || error_exit "Failed to download microvisor image"
 
 else
+    # Check if the correct number of arguments is provided for direct mode
+    if [ "$#" -ne 2 ]; then
+        error_exit "Usage: $0 <Direct_path_to_Microvisor_image> <SHA256_checksum>"
+    fi
+
     # Direct path mode
     IMAGE_PATH="$1"
     SHA_ID="$2"
