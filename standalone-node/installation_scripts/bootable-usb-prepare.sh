985765cca12ca061a97d4f4006436e7a726c9a75
Nayak, Ipsita <ipsita.nayak@intel.com>
2025-07-15 04:18:17 -0700

    shell check fix

--- standalone-node/installation_scripts/bootable-usb-prepare.sh
+++ standalone-node/installation_scripts/bootable-usb-prepare.sh
@@ -113,7 +113,7 @@ fi
 # Ask the password from cmd line 
 echo -n "Please Set the Password for $user_name: "
 stty -echo
-read password
+read -r password
 stty echo
 echo 
 
@@ -124,7 +124,7 @@ chmod 600 "$(pwd)/.psswd"
 # Validate the custom-cloud-init section
 if ! dpkg -s python3 > /dev/null 2>&1; then
     apt install -y python3 > /dev/null 2>&1
-    if [ "$?" -ne 0 ]; then
+    if ! apt install -y python3 > /dev/null 2>&1; then
         echo "Pyhon installation failed,please check!!"
     fi
 fi
@@ -136,7 +136,7 @@ START_MARKER="^services:"
 YAML_CONTENT=$(awk "/$START_MARKER/ {found=1} found" "$CONFIG_FILE")
 
 # Validate using Python
-echo "$YAML_CONTENT" | python3 -c '
+if ! echo "$YAML_CONTENT" | python3 -c '
 import sys, yaml
 
 try:
@@ -158,9 +158,7 @@ try:
 except yaml.YAMLError as e:
     print("Custom cloud-init YAML is invalid:\n", e)
     sys.exit(1)
-'
-# Catch the Error
-if [ "$?" -ne 0 ]; then
+'; then
     echo "Custome cloud-init file is not valiad,Please check!!"
     exit 1
 fi
@@ -340,7 +338,7 @@ copy_files() {
         echo "K3-Cluster scripts done!"
     fi
     # Remove the hidden password file
-    rm -rf $(pwd)/.psswd
+    rm -rf "$(pwd)/.psswd"
 }
 copy_user_apps() {
     echo "Copying user-apps to USB device..."
@@ -350,11 +348,11 @@ copy_user_apps() {
             USER_APPS_PART="p$USER_APPS_PART"
         fi
 	check_mnt_mount_exist
-        mount ${USB_DEVICE}${USER_APPS_PART} /mnt
+        mount "${USB_DEVICE}${USER_APPS_PART}" /mnt
         # Use rsync for fater copr
 	if dpkg -s rsync; then 
 	    rsync -ah --inplace user-apps /mnt/
-	    if [ "$?" -eq 0 ]; then 
+	    if rsync -ah --inplace user-apps /mnt/; then 
 	        echo "user-apps data copied successfully"
 	    else
 		echo "user-apps data failes to copy please check!!"
@@ -363,7 +361,7 @@ copy_user_apps() {
 	    fi
 	else 
 	    cp -r user-apps /mnt
-	    if [ "$?" -eq 0 ]; then
+	    if cp -r user-apps /mnt; then
                 echo "user-apps data copied successfully"
             else
                 echo "user-apps data failes to copy please check!!"
